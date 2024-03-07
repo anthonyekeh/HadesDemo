89801d2f94f35ac2e6201fd87ea9e3008b3b5540
anthonyekeh <anthonyekehjunior@gmail.com>
2024-03-07 17:19:26 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+
+const app = express();
+const port = 3000;
+
+// MySQL Connection
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'anthony',
+    password: 'Huawei@1234##',
+    database: 'users'
+});
+
+db.connect((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log('Connected to MySQL database');
+});
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// Routes
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/submit', (req, res) => {
+    const { name, age, email } = req.body;
+    const newUser = { name, age, email };
+    const sql = 'INSERT INTO login SET ?';
+
+    db.query(sql, newUser, (err, result) => {
+        if (err) {
+            console.error('Error inserting user into database:', err);
+            return res.status(500).send('An error occurred while processing your request.');
+        }
+
+        console.log('User added to database:', result);
+        res.redirect('/');
+    });
+});
+
+
+// Start server
+app.listen(port, () => {
+    console.log(`Server is listening on port 3000`);
+});
