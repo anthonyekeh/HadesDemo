1e68e016ecb3e2cb4dd7a1dfb2b4f83e054cb2c5
anthonyekeh <137506689+anthonyekeh@users.noreply.github.com>
2024-04-25 13:45:14 +0100

    Update app.js

--- app.js
+++ app.js
@@ -8,5 +8,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
+  console.log(`Server running at http://localhost:3000/`);
 });
